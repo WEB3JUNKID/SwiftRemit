7d1490a15e5bf3a6a8a74cd321413fbe09af8712
WEB3JUNKID <victoruchenna2007@gmail.com>
2025-07-26 18:28:42 +0100

    Add files via upload

---
+++ SwiftRemit/client/src/App.tsx
@@ -0,0 +1,37 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Landing from "@/pages/landing";
+import Signup from "@/pages/signup";
+import Login from "@/pages/login";
+import Home from "@/pages/home";
+import Settings from "@/pages/settings";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Landing} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/login" component={Login} />
+      <Route path="/home" component={Home} />
+      <Route path="/settings" component={Settings} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
